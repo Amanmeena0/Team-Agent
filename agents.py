a5fcff2157ff17551e28f45729b3bb150de0a842
AMAN MEENA <141953689+Amanmeena0@users.noreply.github.com>
2026-03-29 20:13:24 +0530

    add intraction with agent part and runing the conversation

--- agents.py
+++ agents.py
@@ -17,9 +17,15 @@ from model import config,model_registry
 from tools import weather_tools
 
 
+def get_resilient_model():
+    try:
+        return model_registry.get_model("fast")
+    except:
+        return model_registry.get_model("balanced")
+
 weather_agent = Agent(
     name = "weather_agent_v1",
-    model=model_registry.get_model("fast"),
+    model=get_resilient_model(),
     description="Provides weather information for specific cities.",
     instruction="You are a helpful weather assistant. "
                 "When the user asks for the weather in a specific city, "
@@ -37,14 +43,6 @@ session_service = InMemorySessionService()
 APP_NAME = "weather_tutorial_app"
 USER_ID = "user_1"
 SESSION_ID = "session_01"
-
-# session = await session_service.create_session(
-#     app_name=APP_NAME,
-#     user_id=USER_ID,
-#     session_id=SESSION_ID,
-# )
-# print(f"Session created: App='{APP_NAME}', User='{USER_ID}', Session='{SESSION_ID}'")
-
 
 async def init_session(app_name:str,user_id:str,session_id:str) -> InMemorySessionService:
     session = await session_service.create_session(
@@ -65,3 +63,38 @@ runner = Runner (
 )
 
 print(f"Runner created for agent '{runner.agent.name}'.")
+
+
+async def call_agent_async(query: str, runner, user_id, session_id):
+    print(f"\n>>> User Query: {query}")
+
+    content = types.Content(role='user', parts=[types.Part(text=query)])
+
+    final_response_text = "Agent did not produce a final response."  # default
+
+    async for event in runner.run_async(
+        user_id=user_id,
+        session_id=session_id,
+        new_message=content
+    ):
+        if event.is_final_response():
+            if event.content and event.content.parts:
+                final_response_text = event.content.parts[0].text
+            elif event.actions and event.actions.escalate:
+                final_response_text = f"Agent escalated: {event.error_message or 'No specific message'}"
+            break
+
+    print(f"<<< Agent Response: {final_response_text}")
+
+
+async def run_convertsation():
+    await call_agent_async("what is the weather like in London?",runner=runner, user_id=USER_ID, session_id=SESSION_ID)
+    await call_agent_async("what is the weather like in Paris?",runner=runner, user_id=USER_ID, session_id=SESSION_ID)
+    await call_agent_async("what is the weather like in New York?",runner=runner, user_id=USER_ID, session_id=SESSION_ID)
+
+
+if __name__ == "__main__":
+    try:
+        asyncio.run(run_convertsation())
+    except Exception as e:
+        print(f"An error occurred: {e}")
